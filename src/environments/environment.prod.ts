294af3185b5dd52277d7f1fab78863f3dee5b4e3
Orel Perez Camejo <orelperezcamejo@gmail.com>
2022-10-19 00:39:21 +0200

    Fin Seccion 7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  URL_TEAM: 'https://angular-portafolio-75076-default-rtdb.firebaseio.com/equipo.json',
+  URL_INFO_PAGE: 'assets/data/data-pagina.json',
+  URL_PRODUCTOS: 'https://angular-portafolio-75076-default-rtdb.firebaseio.com/productos_idx.json',
+  URL_ITEMS: 'https://angular-portafolio-75076-default-rtdb.firebaseio.com/productos/'
 };
